720e19164c13bba5c94c1319623fcf76b1589878
pdostal <git@pdostal.cz>
2013-12-07 14:17:42 +0100

    Fix

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
   require_once('config.php');
-  $file = 'index/'.trim(htmlspecialchars(htmlspecialchars_decode($_GET['file'], ENT_NOQUOTES), ENT_NOQUOTES));
+  $file = 'files/'.trim(htmlspecialchars(htmlspecialchars_decode($_GET['file'], ENT_NOQUOTES), ENT_NOQUOTES));
   require_once('getfile.php');
   exit;
 ?>
