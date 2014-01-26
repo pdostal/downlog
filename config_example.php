815d985d7d0c9d15b77f1f386537231caaa758c0
pdostal <git@pdostal.cz>
2014-01-26 22:10:46 +0100

    Move all back to one folder

---
+++ config_example.php
@@ -0,0 +1,8 @@
+<?php
+	error_reporting(0);
+	$config['server'] = '';
+	$config['database'] = '';
+	$config['username'] = '';
+	$config['password'] = '';
+	$config['table'] = '';
+?>
--- download/config_example.php
+++
@@ -1,8 +0,0 @@
-<?php
-	error_reporting(0);
-	$config['server'] = '';
-	$config['database'] = '';
-	$config['username'] = '';
-	$config['password'] = '';
-	$config['table'] = '';
-?>
